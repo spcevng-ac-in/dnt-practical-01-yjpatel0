a3b6edf587b4e201ef7cf66940d1e3e59a24d16c
yjpatel0 <61009655+yjpatel0@users.noreply.github.com>
2022-04-27 11:42:07 +0530

    Add files via upload

---
+++ dnt-practical-01main/sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            while (number > 0)
+            {
+                int rem = number % 10;
+                sum += rem;
+                number /= 10;
+            }
+            Console.WriteLine("Sum=" + sum);
+            Console.Read();
+        }
+    }
+}
